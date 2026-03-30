afaffd71eed06dffa3af352cc8be5f4a28ebe87e
LucasGabrieldaCosta123 <lucasdavi10001@gmail.com>
2026-03-29 22:21:40 -0300

    Add console logs to exemplo.js for testing

--- plugins/exemplo.js
+++ plugins/exemplo.js
@@ -1,1 +1,5 @@
-
+// plugins/teste.js
+console.log("------------------------------------");
+console.log("Plugin de Teste do TUX carregado!");
+console.log("Instalado por: Lucas");
+console.log("------------------------------------");
